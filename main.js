ab6f60d2687e89c46176e95cddabeee2ee92594e
Andrea-Paperini <etabeta99@live.it>
2019-11-29 19:35:28 +0100

    Verificare se il numero inserito dall'utente è un numero primo

---
+++ main.js
@@ -0,0 +1,24 @@
+var numero = prompt("Inserisci un numero");
+function Primo (n) {
+
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+console.log(Primo(numero));
